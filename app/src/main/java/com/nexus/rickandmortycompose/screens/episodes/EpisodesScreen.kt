7d179648c7515376f24ad91ea8ff5d5990b77382
Daniel Agbemava <danagbemava@gmail.com>
2023-01-08 21:54:31 +0000

    chore: minor modifications

--- app/src/main/java/com/nexus/rickandmortycompose/screens/episodes/EpisodesScreen.kt
+++ app/src/main/java/com/nexus/rickandmortycompose/screens/episodes/EpisodesScreen.kt
@@ -1,23 +1,25 @@
 package com.nexus.rickandmortycompose.screens.episodes
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.nexus.rickandmortycompose.models.dummyEpisode
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
-import org.intellij.lang.annotations.JdkConstants
 
 @ExperimentalMaterial3Api
 @RootNavGraph(start = true)
@@ -35,21 +37,38 @@ fun EpisodesScreen() {
         topBar = {
             LargeTopAppBar(
                 title = {
-                   Text(text = "Episodes")
+                    Text(text = "Episodes")
                 },
                 scrollBehavior = scrollBehavior
             )
         }
     ) {
 
-        when(screenState.value) {
+        when (screenState.value) {
             is EpisodeListScreenState.Loading -> {
-                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = CenterHorizontally) {
-                    Text(text = "loading")
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    CircularProgressIndicator()
                 }
             }
             is EpisodeListScreenState.Error -> {
+                val error = (screenState.value as EpisodeListScreenState.Error).error
 
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = error)
+                    Button(onClick = {
+
+                    }) {
+                        Text(text = "Retry")
+                    }
+                }
             }
             is EpisodeListScreenState.Success -> {
                 val data = (screenState.value as EpisodeListScreenState.Success).episodes
