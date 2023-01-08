3b484292a39528ee5b85acc738c2c5f7b011b879
Daniel Agbemava <danagbemava@gmail.com>
2023-01-08 21:54:41 +0000

    chore: base scaffold

--- app/src/main/java/com/nexus/rickandmortycompose/ui/composables/BaseScaffold.kt
+++ app/src/main/java/com/nexus/rickandmortycompose/ui/composables/BaseScaffold.kt
@@ -1,1 +1,32 @@
-package com.nexus.rickandmortycompose.ui.composablesimport androidx.compose.foundation.layout.PaddingValuesimport androidx.compose.material3.ExperimentalMaterial3Apiimport androidx.compose.material3.Scaffoldimport androidx.compose.runtime.Composableimport androidx.navigation.NavHostControllerimport com.nexus.rickandmortycompose.screens.appCurrentDestinationAsStateimport com.nexus.rickandmortycompose.screens.destinations.Destinationimport com.nexus.rickandmortycompose.screens.startAppDestinationimport com.ramcosta.composedestinations.spec.Route@OptIn(ExperimentalMaterial3Api::class)@Composablefun BaseScaffold(    startRoute: Route,    navController: NavHostController,    topBar: @Composable () -> Unit,    bottomNavBar: @Composable (Destination) -> Unit,    content: @Composable (PaddingValues) -> Unit) {    val destination = navController.appCurrentDestinationAsState().value        ?: startRoute.startAppDestination    Scaffold(        topBar = topBar,        bottomBar = {            bottomNavBar(destination)        },        content = content    )}
+package com.nexus.rickandmortycompose.ui.composables
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.nexus.rickandmortycompose.screens.appCurrentDestinationAsState
+import com.nexus.rickandmortycompose.screens.destinations.Destination
+import com.nexus.rickandmortycompose.screens.startAppDestination
+import com.ramcosta.composedestinations.spec.Route
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BaseScaffold(
+    startRoute: Route,
+    navController: NavHostController,
+    topBar: @Composable () -> Unit,
+    bottomNavBar: @Composable (Destination) -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    val destination = navController.appCurrentDestinationAsState().value
+        ?: startRoute.startAppDestination
+
+    Scaffold(
+        topBar = topBar,
+        bottomBar = {
+            bottomNavBar(destination)
+        },
+        content = content
+    )
+}
